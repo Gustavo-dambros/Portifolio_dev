2af5011a73027373f0d1c34f0bbfab44048ba219
Gustavo-dambros <gustavodambros.19@gmail.com>
2025-12-18 21:43:44 -0300

    .paleta

--- meu-projeto/tailwind.config.js
+++ meu-projeto/tailwind.config.js
@@ -2,7 +2,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        primaria: '#ff0000',
+        bg_color: '#001B1E',
+       red:'#DE755B',
+       blue: '#2D5AE1',
+       green: '#A6E05A',
       },
     },
   },
