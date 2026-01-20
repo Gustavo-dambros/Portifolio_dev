e095b68c6a83e0b0896b43f724d093ee07d30602
Gustavo-dambros <gustavodambros.19@gmail.com>
2026-01-20 08:30:13 -0300

    iniciando

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ export default {
     extend: {
       colors: {
         bg_color: '#001B1E',
-       red:'#DE755B',
-       blue: '#2D5AE1',
-       green: '#A6E05A',
+       
       },
     },
   },
